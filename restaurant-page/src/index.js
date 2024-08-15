d431d5c914f22f80c75cd9cbd14d61e762c7873d
Samyak Raj Shakya <108289345+samekkk@users.noreply.github.com>
2024-08-15 16:20:16 +0545

    Delete restaurant-page directory

--- restaurant-page/src/index.js
+++
@@ -1,7 +0,0 @@
-import './styling/style.css';
-import './styling/menu.css';
-import './styling/about.css';
-import initial_page from './component/initial-page.js';
-
-console.log("jellep");
-initial_page();
